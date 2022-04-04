033b9d081f8ca436b4143e48b65586008e536e52
atmason90 <atmason90@gmail.com>
2022-04-03 18:30:57 -0700

    write quiz questions in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// create a variable that holds all questions, choices, and correct answers
+var questions = [
+    {
+        question: "Javascript code is run on the _________.",
+        choices: ["Code Editor", "Browser", "Local Machine", "Repository"],
+        answer: "Browser"
+    },
+    {
+        question: "What are the two possible boolean values?",
+        choices: ["True or False", "Positive or Negative", "String or Array", "Equal or Not Equal"],
+        answer: "True or False"
+    },
+    {
+        question: "When writing an array, what symbols should it be contained within?",
+        choices: ["()", "{}", "<>", "[]"],
+        answer: "[]"
+    },
+    {
+        question: "What is the index position of 'train' in the following array? ['bike', 'car', 'bus', 'train', 'plane']",
+        choices: ["1", "2", "3", "4"],
+        answer: "3"
+    },
+    {
+        question: "What does '===' mean in javascript when comparing two things?",
+        choices: ["They have the same value", "They are the same type", "They are the same value and type", "They are the same value, type, and style"],
+        answer: "They are the same value and type"
+    },
+];
