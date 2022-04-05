7c6f4fea3396739b599b0b64e8cf5c73f29a20ba
atmason90 <atmason90@gmail.com>
2022-04-04 22:48:55 -0700

    add comments to highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,25 +1,33 @@
 
+// declare variables needed
 var scores = document.querySelector("#scores");
 var clearScores = document.querySelector("#clear-scores");
 var backButton = document.querySelector("#back-button");
 
+// add event listener to clear scores button that listens for a click and then runs a function to clear local storage
 clearScores.addEventListener("click", function() {
     localStorage.clear();
     location.reload();
 });
 
+// retrieve all scores from local storage
 var allScores = localStorage.getItem("allScores");
+// use JSON.parse to de-stringify all scores objects in local storage
 allScores = JSON.parse(allScores);
 
+// if all scores is not null, run for loop to iterate through all scores
 if (allScores !== null) {
     for (var i = 0; i < allScores.length; i++) {
         
+        // create li for each individual score object (initials, score)
         var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " - " + allScores[i].score;
+        // append to page
         scores.appendChild(createLi);
     }
 }
 
+// add event listener that listens for click and navigates back to the quiz, where user can retake
 backButton.addEventListener("click", function() {
     window.location.replace("./index.html");
 });
